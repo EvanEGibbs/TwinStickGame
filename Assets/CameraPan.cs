aa4cf4e54d335cb2f0c036c50006dac4cbefde28
Evan Gibbs <evanegibbs@gmail.com>
2017-05-05 00:39:29 -0700

    Added Second Stick Camera Control

---
+++ Assets/CameraPan.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPan : MonoBehaviour {
+
+	private GameObject playerObject;
+	//private Vector3 startCoordinates;
+
+	void Start () {
+		playerObject = GameObject.FindGameObjectWithTag("Player");
+		//startCoordinates = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+	}
+	
+	void LateUpdate () {
+		//transform.position = startCoordinates + playerObject.transform.position;
+		transform.LookAt(playerObject.transform);
+	}
+}
